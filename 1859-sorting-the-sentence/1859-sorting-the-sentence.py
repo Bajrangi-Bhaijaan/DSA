ba5fe5461a5cffcd987d853d633ffea43754d44b
Partha Pratim Kalita <102345589+Bajrangi-Bhaijaan@users.noreply.github.com>
2022-06-24 17:56:10 +0530

    Time: 49 ms (5.83%), Space: 13.5 MB (47.22%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,13 +1,11 @@
-class Solution(object):
-    def sortSentence(self,s):
-        words = s[::-1].split()
-        words.sort()
-        result = [ word[1:][::-1] for word in words ]
-        return ' '.join(result)
+class Solution:
+    def sortSentence(self, s):
         
+        splited_string = s[::-1].split() # here first we are reversing the sting and then spliting it, split() function make each word of the string as a separate element of the list.  For example: ['3a', '1sihT', '4ecnetnes', '2si']
+        splited_string.sort() # as we are having number in front of each word now, we can sort the list.
         
-        """
-        :type s: str
-        :rtype: str
-        """
+        res = [] # taking empty list to save the result. 
         
+        for word in splited_string: # travering the splited string. 
+            res.append(word[1:][::-1]) # here by doing "[1:]" we are eradicating number from the word & by doing "[::-1]" we are reversing back the word, that we reversed at one step of the solution. Here res will have "['This', 'is', 'a', 'sentence']  
+        return " ".join(res) # in res we are having list of words, now we want a string with words separated -> "This is a sentence". 
